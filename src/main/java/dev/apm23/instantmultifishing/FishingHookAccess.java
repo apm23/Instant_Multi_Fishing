cda96c1ffc3bd8c81dc5fda9ce5f12dc635f176b
apm23 <80390905+apm23@users.noreply.github.com>
2026-09-07 01:59:12 +0900

    Add fishing hook state access contract

---
+++ src/main/java/dev/apm23/instantmultifishing/FishingHookAccess.java
@@ -0,0 +1,6 @@
+package dev.apm23.instantmultifishing;
+
+public interface FishingHookAccess {
+    void instantMultiFishing$setNibble(int value);
+    void instantMultiFishing$setOpenWater(boolean value);
+}
